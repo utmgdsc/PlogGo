e2086e49c2efa612004f7b043237d1a804eeee72
HweyTH <thhuy1101@gmail.com>
2025-02-05 11:28:22 -0500

    add MongoDB connection test script

---
+++ app/test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config['MONGO_URI']="mongodb://localhost:27017/test_ploggo"
+mongo=PyMongo(app)
+
+# perform a test query
+try:
+    mongo.db.users.find_one() # forces a connection
+    print("Connected succesfully to MongoDB!")
+except Exception as e:
+    print(f"Connection failed: {e}")
